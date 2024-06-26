3eb23b94860fcc335106bf8e59a5d3ee88163b0b
Silvio-Ferreira <150188262+Silvio-Ferreira@users.noreply.github.com>
2024-06-26 19:07:36 -0300

    Projetos
    HTML dos projetos

---
+++ angular-portifolio/src/app/modules/portifolio/components/experiences/experiences.component.ts
@@ -0,0 +1,43 @@
+import { Component, signal } from '@angular/core';
+import { IExperiences } from '../../interfaces/IExperiences.interface';
+
+@Component({
+  selector: 'app-experiences',
+  standalone: true,
+  imports: [],
+  templateUrl: './experiences.component.html',
+  styleUrl: './experiences.component.scss',
+})
+export class ExperiencesComponent {
+  public arrayExperiences = signal<IExperiences[]>([
+    {
+      summary: {
+        strong: 'Blog',
+        //p: '',
+      },
+      text: 'Um blog pessoal desenvolviedo com Angular, TypeScript, HTML e CSS.',
+    },
+    {
+      summary: {
+        strong: 'Busca CEP',
+       // p: '',
+      },
+      text:'O projeto "Consulta-CEP" é uma aplicação web que permite aos usuários pesquisar códigos postais brasileiros (CEP) e obter informações detalhadas do endereço. As tecnologias utilizadas foram HTML, CSS e JavaScript.',
+  
+    },
+    {
+      summary: {
+        strong: 'Gerenciador de Contatos',
+       // p: '',
+      },
+      text: 'É um sistema web de gerenciamento de contatos desenvolvido em C# e ASP.NET Core MVC. Ele possibilita aos usuários adicionar, editar e excluir informações de contato, como nomes, números de telefone e endereços de email. As tecnologias utilizadas incluem C#, ASP.NET Core MVC, Entity Framework, HTML, CSS e JavaScript.',
+    },
+    {
+      summary: {
+        strong: 'Quiz',
+        //p: '',
+      },
+      text: 'O projeto "Angular BuzzFeed Quiz" é uma aplicação de quiz inspirada nos quizzes do BuzzFeed. Ele permite que os usuários respondam a quizzes e obtenham resultados personalizados com base em suas respostas. A aplicação é construída com Angular, TypeScript, HTML e CSS.',
+    }
+  ]);
+}
